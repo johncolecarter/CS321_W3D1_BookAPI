560e95e5684f2a3757aa105e3993918432fcaf55
John Cole Carter <54083803+johncolecarter@users.noreply.github.com>
2020-01-27 18:42:45 -0600

    Add files via upload

---
+++ CS321_W3D1_BookAPI/CS321_W3D1_BookAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS321_W3D1_BookAPI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
